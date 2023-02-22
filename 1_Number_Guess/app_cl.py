e2d26f62ac6127c1622d70f90e1bff0054febfec
SujahathMSM <sujahathmhmd@gmail.com>
2023-02-22 10:49:26 +0530

    Changed the comments

--- 1_Number_Guess/app_cl.py
+++ 1_Number_Guess/app_cl.py
@@ -2,9 +2,10 @@ import random
 
 class NumberGuesser:
     def __init__(self, max_num=100):
-        self.number = random.randint(1, max_num) #picking a randomnumber between 1 and max_num
+        #picking a randomnumber between 1 and max_num
+        self.number = random.randint(1, max_num) 
         self.max_num = max_num
-        self.num_guesses = 0 # initialising the number of guesses to 0
+        self.num_guesses = 0 
         
     def guess(self, number):
         self.num_guesses += 1
@@ -16,21 +17,26 @@ class NumberGuesser:
             return "You got it!"
 
 if __name__ == "__main__":
-    max_num = int(input("Enter the maximum number for the game: ")) #getting the maximum number from the user
+    #getting the maximum number from the user
+    max_num = int(input("Enter the maximum number for the game: ")) 
     game = NumberGuesser(max_num) #creating an instance of the NumberGuesser class
     
     while True:
-        guess = int(input(f"Guess a number between 1 and {max_num}: ")) #getting the user's guess
-        result = game.guess(guess) #calling the guess method
-        print(result) #printing the result
+        #getting the user's guess
+        guess = int(input(f"Guess a number between 1 and {max_num}: ")) 
+        #calling the guess method
+        result = game.guess(guess) 
+        print(result) 
         
-        if result == "You got it!": #checking if the user got the number right
-            print(f"It took you {game.num_guesses} guesses.") #printing the number of guesses
+        if result == "You got it!": 
+            #checking if the user got the number right
+            print(f"It took you {game.num_guesses} guesses.") 
             
-            play_again = input("Do you want to play again? (y/n) ") #asking the user if they want to play again
-            if play_again.lower() == "y": #checking if the user wants to play again
-                max_num = int(input("Enter the maximum number for the game: ")) #getting the maximum number from the user
+            play_again = input("Do you want to play again? (y/n) ") 
+            #checking if the user wants to play again
+            if play_again.lower() == "y": 
+                max_num = int(input("Enter the maximum number for the game: ")) 
                 game = NumberGuesser(max_num)
             else:
-                print("Good bye!, will see you next time!") #printing a goodbye message
+                print("Good bye!, will see you next time!") 
                 break
